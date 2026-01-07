9a7837593996bea8b1ad81e89342a800c7c47b8c
2kpi <group.2kpi@gmail.com>
2026-01-07 11:03:45 +0100

    Data

--- data.py
+++ data.py
@@ -13,7 +13,7 @@ torch.manual_seed(SEED)
 jax_key = jax.random.PRNGKey(SEED)
 
 
-# Générer OU signal
+# EXEMPLE  Générer OU signal
 
 def generate_ou_signal(T=500, seed=0, kappa=15, theta=0., nu=1.5):
     rng = np.random.default_rng(seed)
@@ -30,4 +30,18 @@ def generate_ou_signal(T=500, seed=0, kappa=15, theta=0., nu=1.5):
         )
 
     return np.linspace(0, 1, T), x
+
+
+def build_dataset(X_raw, seq_len=50, step=1):
+    Xs, Ys = [], []
+    N, C = X_raw.shape
+    for i in range(N - seq_len - step):
+        Xs.append(X_raw[i:i+seq_len])
+        Ys.append(X_raw[i+step:i+step+seq_len])
+    X = torch.tensor(np.array(Xs), dtype=torch.float32)
+    Y = torch.tensor(np.array(Ys), dtype=torch.float32)
+    return X, Y
+
+
+
 
