90a7494b1e30a81d9ba40847443045de252b099f
2kpi <group.2kpi@gmail.com>
2026-01-22 17:00:21 +0000

    modified

--- src/efmgate/__init__.py
+++ src/efmgate/__init__.py
@@ -1,18 +1,15 @@
-# Réexport des fonctions dataset
-from .dataset import load_dataset, split_data, r2_score
-
+# Nouveau contenu :
+from .data import generate_ou_signal, build_dataset, split_data, r2_score
 from .models.efm_gate import EfmLSTM, EfmLSTMPredictor
-
 
 # -----------------------------
 # Définition de ce qui sera accessible via 'from efmgate import *'
 # -----------------------------
 __all__ = [
-    "load_dataset",
-    "split_data",
-    "r2_score",
-    "EfmLSTM",
-    "EfmLSTMPredictor",
-    # "some_data_function"  # décommenter si tu ajoutes des fonctions dans data
+    'generate_ou_signal', 
+    'build_dataset', 
+    'split_data', 
+    'r2_score',
+    'EfmLSTM', 
+    'EfmLSTMPredictor'
 ]
-
