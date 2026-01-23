4163006e1cd53be600db1e5686c5805e7d51e308
2kpi <group.2kpi@gmail.com>
2026-01-23 19:51:41 +0100

    modified

--- src/efmgate/__init__.py
+++ src/efmgate/__init__.py
@@ -1,5 +1,5 @@
 from efmgate.models import EfmLSTM
-from efmgate.data import generate_ou_signal, split_data, r2_score
+from efmgate.data_utils import generate_ou_signal, split_data, r2_score
 
 __all__ = [
     "EfmLSTM",
