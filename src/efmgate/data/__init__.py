3bc4ebc66a7d69348431b68d0704e5469e069b34
2kpi <group.2kpi@gmail.com>
2026-01-23 19:48:50 +0100

    commit modified

--- src/efmgate/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataset import generate_ou_signal, build_dataset, split_data, r2_score
-
-__all__ = ["generate_ou_signal", "build_dataset", "split_data", "r2_score"]
-
---
+++ src/efmgate/data_utils/__init__.py
@@ -0,0 +1,4 @@
+from .preprocessing import generate_ou_signal, build_dataset, split_data, r2_score
+
+__all__ = ["generate_ou_signal", "build_dataset", "split_data", "r2_score"]
+
