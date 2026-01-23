e8bebc2c893c152d638595dbf61a96ed9272e46a
2kpi <group.2kpi@gmail.com>
2026-01-23 20:36:59 +0100

    modified

--- src/efmgate/__init__.py
+++ src/efmgate/__init__.py
@@ -1,10 +1,11 @@
 from efmgate.models import EfmLSTM
-from efmgate.data_utils import generate_ou_signal, split_data, r2_score
+from efmgate.data_utils import generate_ou_signal,build_dataset, split_data, r2_score
 
 __all__ = [
     "EfmLSTM",
     "generate_ou_signal",
     "split_data",
     "r2_score",
+    "build_dataset"
 ]
 
