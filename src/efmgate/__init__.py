0790b85b86f188f4acc670f62a2bd8a5fa555e5a
2kpi <group.2kpi@gmail.com>
2026-01-25 13:33:02 +0100

    Add signax as part of efmgate repo

--- src/efmgate/__init__.py
+++ src/efmgate/__init__.py
@@ -1,8 +1,9 @@
-from efmgate.models import EfmLSTM
+from efmgate.models import EfmLSTM, SigLSTM
 from efmgate.data_utils import generate_ou_signal,build_dataset, split_data, r2_score
 
 __all__ = [
     "EfmLSTM",
+    "SigLSTM",
     "generate_ou_signal",
     "split_data",
     "r2_score",
